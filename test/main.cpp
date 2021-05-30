690195d7abdb9ca53a1e4881e0c31c80015ca902
Stan O <spammyboy@protonmail.com>
2021-05-30 15:25:08 +0100

    Test structure here (graphic + non-graphic)
    Now all that is left to do is actually right the tests. Graphic and non graphic mode is well structured.

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include <time.h>
-#include <thread>
-#include <string>
 #include <SDL2/SDL.h>
 
 #ifdef _WIN32
@@ -21,18 +17,16 @@
 
 #include "../Ase_Loader/Ase_Loader.h"
 
-struct EngineClock {
+
+// For now using SDL_Delay instead of this, but will probably use this later.
+struct Clock {
     // dt
     float last_tick_time = 0;
-
-    // for average fps
-    float fpss [ACCURACY];
-    int fpss_index = -1;
-    int average_fps = -1;
+    float dt = 0;
 
     void tick() {
         float tick_time = SDL_GetTicks();
-        g_dt = (tick_time - last_tick_time) / 1000;
+        dt = (tick_time - last_tick_time) / 1000;
         last_tick_time = tick_time;
     }
 };
@@ -42,59 +36,105 @@ struct Graphics {
     SDL_Renderer* rdr;
 };
 
+// Expected results
 struct Test {
+    char* file_path;
+    u32 num_slices;
+};
+
+struct TestIter {
     SDL_Texture* texture = NULL;
     Ase_Output* ase_output = NULL;
-    // For when we're in graphics mode
-    bool quit = false;
+    int i = 0; // index
 };
 
 
-void GraphicsLaunch();
-void GraphicsShutdown();
-bool WindowThread();
+void RunIthTest(TestIter* test_iter, Test* tests);
+void GraphicsLaunch(Graphics* g);
+void GraphicsShutdown(Graphics* g);
+bool EventLoop();
 
 
 int main(int argc, char* argv[]) {
 
-    print("%i", argc);
-    print(argv[0]);
-
     bool graphics_mode = false;
-
     if (argc > 1) {
-        if (strcmp(argv[1], "graphic") != 0) {
+        if (strcmp(argv[1], "graphic") == 0) {
             graphics_mode = true;
         }
         else {
+            print("%s", argv[1]);
             print("Invalid arg. Available: graphic");
         }
     }
 
-    Test test_data;
 
+    Test tests [] = {
+        {"tests/slices_1.ase", 2}
+    };
+    TestIter test_iter;
+    int num_tests = sizeof(tests) / sizeof(tests[0]);
+
+    //////// GRAPHICS MODE ////////
     if (graphics_mode) {
-
-        std::thread test_thread(Test, & test_data);
 
         Graphics g;
         GraphicsLaunch(& g);
-        GraphicsLoop(& g, & test_data, & test_thread);
+
+        for (; test_iter.i < num_tests; test_iter.i++) {
+
+            RunIthTest(& test_iter, & tests[0]);
+
+            // Draw the test texture if it actually exists
+            // (checking != NULL on the off chance that a test failes miserably and the texture also fails to load)
+            if (test_iter.texture != NULL) {
+                SDL_RenderClear(g.rdr);
+                SDL_Rect drect = {0, 0, 0, 0};
+                SDL_QueryTexture(test_iter.texture, NULL, NULL, & drect.w, & drect.h);
+                SDL_RenderCopy(g.rdr, test_iter.texture, NULL, & drect);
+
+                SDL_DestroyTexture(test_iter.texture);
+            }
+
+            // If we get an input that we want to quit the program, we do as so.
+
+            // We don't need to free any test related stuff here because we should have freed all ase_loader
+            // related things in Test(), And freed the texture in the if statement above if needed.
+            if (EventLoop()) {
+                GraphicsShutdown(& g);
+                break;
+            }
+
+            SDL_Delay(100);
+        }
+
+        // This lets us have the window open even after we finish all the tests.
+        // This makes it easier to create the ability to cycle back through or pick
+        // specific tests in the future.
+        while (true) {
+            if (EventLoop()) {
+                GraphicsShutdown(& g);
+                break;
+            }
+        }
+
     }
+
+    //////// NON-GRAPHICS MODE ////////
     else {
-        Test(& test_data);
+        for (; test_iter.i < num_tests; test_iter.i++) {
+            RunIthTest(& test_iter, & tests[0]);
+        }
     }
 
     return 0;
 }
 
-void Test(Test* test_data) {
-    while (true) {
-        if (test_data->quit) {
-            print("exiting test loop!");
-            break;
-        }
-    }
+// Tests the ith test.
+// The only thing it doesn't do is SDL_DestroyTexture, because when we are in graphics mode
+// we want to wait until we draw the texture to the screen before we destroy it.
+void RunIthTest(TestIter* test_iter, Test* tests) {
+    print("Test %i %s", test_iter->i, tests[test_iter->i].file_path);
 }
 
 void GraphicsLaunch(Graphics* g) {
@@ -107,49 +147,33 @@ void GraphicsLaunch(Graphics* g) {
     SDL_RenderSetScale(g->rdr, scale, scale);
 }
 
-void GraphicsShutdown() {
-    SDL_DestroyTexture(test_texture);
-    SDL_DestroyWindow(window);
+void GraphicsShutdown(Graphics* g) {
+    SDL_DestroyWindow(g->window);
     IMG_Quit();
     SDL_Quit();
 }
 
-void GraphicsLoop(Graphics* g, Test* test_data) {
+// Returns true if quiting the program
+bool EventLoop() {
 
     bool quit = false;
 
-    while (! quit) {
+    SDL_Event event;
+    while (SDL_PollEvent(& event)) {
 
-        SDL_Event event;
-        while (SDL_PollEvent(& event)) {
-
-            switch (event.type) {
-                case SDL_QUIT:
-                    quit = true;
-                case SDL_KEYDOWN:
-                    switch (event.key.keysym.sym) {
-                        case SDLK_ESCAPE:
-                            quit = true;
-                            break;
-                        default: break;
-                    }
-                default: break;
-            }
-        }
-
-        // Draw the test texture if it actually exists
-        if (test_data->texture != NULL) {
-            SDL_RenderClear(g->rdr);
-            SDL_Rect drect = {0, 0, 0, 0};
-            SDL_QueryTexture(test_data->texture, NULL, NULL, & drect.w, & drect.h);
-            SDL_RenderCopy(g->rdr, test_data->texture, NULL, & drect);
+        switch (event.type) {
+            case SDL_QUIT:
+                quit = true;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE:
+                        quit = true;
+                        break;
+                    default: break;
+                }
+            default: break;
         }
     }
 
-    // If we exit the program before the tests are done, there's a chance that we'll exit while there's a texture that hasn't been freed.
-    // If we free the texture here and it's already been freed, SDL ignores it (the null pointer) and moves on :D.
-    SDL_DestroyTexture(test_data->texture);
-    Ase_Destroy_Output(test_data->ase_output);
-
-    GraphicsShutdown();
+    return quit;
 }
